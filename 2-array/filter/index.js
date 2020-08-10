ad96b8c02fbac540cc3e74d513f76add036173e5
RiverAndBoat <842714673@qq.com>
2020-08-10 19:13:07 +0800

    filter-array

--- 2-array/filter/index.js
+++ 2-array/filter/index.js
@@ -1,9 +1,11 @@
 function chooseMultiplesOfThree(collection) {
   // TODO 1: 在这里写实现代码
+  return collection.filter(c => c % 3 === 0);
 }
 
 function chooseNoRepeatNumber(collection) {
   // TODO 2: 在这里写实现代码
+  return Array.from(new Set(collection));
 }
 
 export { chooseMultiplesOfThree, chooseNoRepeatNumber };
