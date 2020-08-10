58f808917258a0cfcaec9fd7d6b582e735591c52
RiverAndBoat <842714673@qq.com>
2020-08-10 21:39:24 +0800

    async-fetch

--- 5-async/3-fetch/index.js
+++ 5-async/3-fetch/index.js
@@ -2,6 +2,12 @@ function fetchData(url) {
   // <-- start
   // TODO 23: 通过Fetch API实现异步请求
   // end -->
+  return fetch(url).then(res => {
+    if (res.status === 200) {
+      return res.json();
+    }
+    return res.statusText;
+  });
 }
 
 const URL = 'http://localhost:3000/api';
