2ddc5801125a19fa57fa8297d6a8217d9b514a7d
RiverAndBoat <842714673@qq.com>
2020-08-10 19:48:06 +0800

    object-values

--- 3-object/values/index.js
+++ 3-object/values/index.js
@@ -1,3 +1,5 @@
 export default function countTypesNumber(source) {
   // TODO 6: 在这里写实现代码
+  let values = Object.values(source);
+  return values.reduce((prev, next, index, array) => Number(prev) + Number(next));
 }
