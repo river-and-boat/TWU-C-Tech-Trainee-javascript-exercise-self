e3deceb585364dea8cda0598be7e8ea5723642ae
RiverAndBoat <842714673@qq.com>
2020-08-10 22:24:20 +0800

    finish work

--- 3-object/assign/index.js
+++ 3-object/assign/index.js
@@ -1,5 +1,8 @@
 export default function addSerialNumber(source) {
   // TODO 5: 在这里写实现代码，需要采用ES6 Object.assign
+  // eslint-disable-next-line no-param-reassign
   source.serialNumber = '12345';
+  // eslint-disable-next-line no-param-reassign
+  source.properties.status = 'processed';
   return source;
 }
--- 3-object/values/index.js
+++ 3-object/values/index.js
@@ -1,5 +1,6 @@
 export default function countTypesNumber(source) {
   // TODO 6: 在这里写实现代码
-  let values = Object.values(source);
+  const values = Object.values(source);
+  // eslint-disable-next-line no-unused-vars
   return values.reduce((prev, next, index, array) => Number(prev) + Number(next));
 }
