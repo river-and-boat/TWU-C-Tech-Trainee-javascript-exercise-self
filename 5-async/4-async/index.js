5b302b441671d129831695999a8181e1808be84e
RiverAndBoat <842714673@qq.com>
2020-08-10 21:57:42 +0800

    async-async

--- 5-async/4-async/index.js
+++ 5-async/4-async/index.js
@@ -1,6 +1,13 @@
 async function fetchData(url) {
   // <-- start
   // TODO 24: 通过await/async实现异步请求
+  const res = await fetch(url);
+  if (res.status === 200) {
+    const response = await res.json();
+    document.writeln(response.name);
+  } else {
+    console.error(res.statusText);
+  }
   // end -->
 }
 
